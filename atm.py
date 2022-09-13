508bdea59235952a47ac953f5276d955da3d764d
sivabalaji525 <113123862+sivabalaji525@users.noreply.github.com>
2022-09-13 08:42:10 +0530

    Update atm.py

--- atm.py
+++ atm.py
@@ -1,6 +1,7 @@
 import mysql.connector
-from mysql.connector import Error
+from mysql.connector import Error #To ignore all the error
 
+#creating connection to the database:
 mydb=mysql.connector.connect(
     host="localhost",
     user="root",
@@ -15,6 +16,7 @@ mycursor = mydb.cursor()
 print("Welcome to atm")
 n=int(input("Press 1 for login and 0 for register"))
 
+#Declaring variables globally
 password=""
 myname=""
 depo=""
@@ -26,6 +28,7 @@ if n==0:
     t=int(input("Enter Deposit Amount"))
     pw=int(input("Set Your Password:"))
 
+    #Four parameters will be stored in four variables.
     myname=name
     password=pw
     deposit=t
@@ -33,11 +36,69 @@ if n==0:
 
     val=(myname,password,depo,id)
 
-    sql= "insert into data(my_name, pass_word, de_po, i_d) values(%s,%s,%s,%s) "
+    #sql query is executed.
+    sql= """insert into data(my_name, pass_word, de_po, i_d) 
+           values(%s,%s,%s,%s) """
 
-   
     mycursor.execute(sql,val)
+
+    #Apply changes that we created in sql program.
     mydb.commit()
+    #display success if there is no error
     print("success")
+
+if n==1:
+    info= int(input("Enter ur id: "))
+    passw=int(input("Enter the password: "))
+
+    #info is the id  given by the user
+    mycursor.execute("select * from atm.data where i_d=%s" %(info))
+
+    #fetchone() is used to fetch the row 
+    row=mycursor.fetchone()
+
+    if mycursor.rowcount==1:
+
+        #verifying the password given by the user
+        mycursor.execute("select * from atm.data where pass_word= %s"%(passw))
+        row=mycursor.fetchone()
+
+        if mycursor.rowcount==1:
+            print("login successful")
+            d=int(input("Enter 1 for withdrawl or 0 for deposit and 3 for exit: "))
+
+            if d==1:
+                a= int(input("How much you want to withdrawl?: "))
+
+                #passw is the password given by the user previously
+                mycursor.execute("select de_po from atm.data where pass_word=%s"%(passw))
+                col=mycursor.fetchone()
+                x=list(col)
+
+                for i in x:
+
+                    #z is current amout
+                    z=(int(i))
+
+                    #a is the withdrawal amount
+                    #all the updates are stored in c
+                    c=z+a
+
+                #Update the current amount after transaction.    
+                mycursor.execute("update data set de_po=%s"%(c,passw))
+
+            if d==3:
+                #Exit the program
+                exit(0)
+
+        else:
+            print("invalid password")
+
+    else:
+        print("Account does't exist") 
+
+    mydb.commit()   
     
+ 
+
  
